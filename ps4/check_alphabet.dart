c3a676df8c65f1d8ba0076a0028be7a58d462905
Abdallah akl <dalla.dd123@yahoo.com>
2026-05-04 07:37:56 +0200

    check whether an alphabet is a vowel or consonant

---
+++ ps4/check_alphabet.dart
@@ -0,0 +1,42 @@
+// Write a  program to check whether an alphabet is a vowel or consonant.
+// Vowel alphabet is ( الحروف المتحركه ):(a,i,o,u,e)
+// Consonant : rest of alphabets
+
+// _____________________________________________________________________________
+// _____________________________________________________________________________
+
+import 'dart:io';
+
+void main() {
+  stdout.write('Enter alphabet; ');
+  String alphabet = stdin.readLineSync()!.toLowerCase();
+  checkAlphabet(alphabet );
+}
+
+// _____________________________________________________________________________
+// _____________________________________________________________________________
+
+void checkAlphabet1(String alpha) {
+  List alphabets = ['a', 'i', 'o', 'u', 'e'];
+  if (alphabets.contains(alpha)) {
+    print('The alphabet is a vowel');
+  } else {
+    print('The alphabet is a consonant');
+  }
+}
+
+void checkAlphabet(String alphabet) {
+  if (alphabet.length != 1) {
+    print('Please enter only one character');
+  } else if (int.tryParse(alphabet) != null) {
+    print('Please enter one String Value');
+  } else if (alphabet == 'a' ||
+      alphabet == 'i' ||
+      alphabet == 'o' ||
+      alphabet == 'u' ||
+      alphabet == 'e') {
+    print('The alphabet is a vowel ');
+  } else {
+    print('The alphabet is a consonant');
+  }
+}
